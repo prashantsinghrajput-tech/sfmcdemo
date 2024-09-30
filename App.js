28e74dfee53797048494b033a3359c8b716a25bd
prashantsinghrajput-tech <prashantsinghkaramana@gmail.com>
2024-09-30 12:50:53 +0530

    sfmc demo react native

---
+++ App.js
@@ -0,0 +1,147 @@
+import React, {useRef} from "react";
+import { Button, View, Text, NativeModules, NativeEventEmitter, Alert, Image } from "react-native";
+import { NavigationContainer, useNavigationContainerRef } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const { InteractionStudioModule } = NativeModules;
+const Stack = createNativeStackNavigator();
+
+/**
+ * Simple app with 2 screens. The home screen has a button which sends an 
+ * event to Interaction Studio when clicked and update the UI with campaign response data
+ */
+function App() {
+
+    const navigationRef = useNavigationContainerRef();
+    const routeNameRef = useRef();
+
+    return (
+        <NavigationContainer
+            ref={navigationRef}
+            onReady={() => {
+                const currentRouteName = navigationRef.getCurrentRoute().name;
+                routeNameRef.current = currentRouteName;
+                InteractionStudioModule.viewScreen(currentRouteName, null); // initial view
+                console.log("View " + currentRouteName);
+            }}
+            onStateChange={async () => {
+                /*
+                 * If screen changes, call native module to send event to Interaction Studio
+                 */
+                const previousRouteName = routeNameRef.current;
+                const currentRouteName = navigationRef.getCurrentRoute().name;
+                if (previousRouteName !== currentRouteName) {
+
+                    if (currentRouteName === "Product") { // View Product
+                        let productId = "abc"; // lookup product id
+                        InteractionStudioModule.viewScreen(currentRouteName, productId);
+                    } else if (currentRouteName === "Category") { // View Category
+                        let categoryId = "abc"; // lookup category id
+                        InteractionStudioModule.viewScreen(currentRouteName, categoryId);
+                    } else {
+                        InteractionStudioModule.viewScreen(currentRouteName, null);
+                    }
+                    console.log("View " + currentRouteName);
+
+                }
+                routeNameRef.current = currentRouteName;
+            }}
+        >
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Product" component={ProductScreen} />
+                 <Stack.Screen name="Category" component={CategoryScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+/**
+ * Category screen
+ */
+function CategoryScreen({ navigation: { goBack, navigate } }) {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Button title="Go Back" onPress={() => goBack() } />
+        </View>
+    );
+}
+
+/**
+ * Product screen with an add to cart button
+ */
+function ProductScreen({ navigation: { goBack, navigate } }) {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Button title="Add to Cart" onPress={() => {
+                let productId = "abc"; // lookup product id
+                let quantity = 1; // set quantity
+                InteractionStudioModule.addToCart(productId, quantity, () => {
+                    Alert.alert( null, "Product has been added to cart." );
+                });
+            } }
+            />
+            <Button title="View Category" onPress={() => navigate('Category') } />
+            <Button title="Go to Home" onPress={() => navigate('Home') } />
+        </View>
+    );
+}
+
+/**
+ * Home screen
+ */
+class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            headerText: null,
+            imageURL: null
+        };
+    }
+
+    /**
+      * When the component is ready, add an event listener to handle home banner 1 campaign response
+      */ 
+//    componentDidMount() {
+//        const eventEmitter = new NativeEventEmitter(InteractionStudioModule);
+//        this.eventListener = eventEmitter.addListener("homeBanner1_ready", (eventData) => {
+//            console.log(eventData.imageURL);
+//            this.setState({
+//                imageURL: eventData.imageURL,
+//                headerText : eventData.headerText
+//            });
+//        });
+//    }
+//
+//    componentWillUnmount() {
+//        this.eventListener.remove(); //Removes the listener
+//    }
+
+    /**
+     * Renders component
+     */
+    render() {
+        let isLoading = ( this.state.imageURL == null || this.state.headerText == null );
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                { isLoading ? 
+                    ( <Text style={{marginBottom: 5}}>Loading...</Text> ) : 
+                    ( 
+                        <>
+                            <Text style={{ fontWeight: 'bold', fontSize: 24, marginBottom: 5 }}>{this.state.headerText}</Text>
+                            <Image source={{ uri: this.state.imageURL }} style={{ resizeMode: 'cover', width: '100%', height: 200, marginBottom: 5 }} />
+                        </>
+                    )
+                }
+                <Button
+                    title="Go to Product"
+                    onPress={() => {
+                        this.props.navigation.navigate("Product");
+                    }}
+                />
+            </View>
+        );
+    }
+}
+
+export default App;
---
+++ android/app/src/main/java/com/demomcpapp/InteractionStudioModule.java
@@ -0,0 +1,131 @@
+package com.demomcpapp;
+
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.evergage.android.Campaign;
+//import com.evergage.android.CampaignHandler;
+import com.evergage.android.Evergage;
+import com.evergage.android.Context;
+import com.evergage.android.Screen;
+import com.evergage.android.promote.Product;
+import com.evergage.android.promote.Category;
+import com.evergage.android.promote.LineItem;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
+import org.json.JSONObject;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+
+/**
+ * Module used to expose Interaction Studio Mobile SDK to React Native JavaScript
+ * See https://reactnative.dev/docs/native-modules-android
+ */
+class InteractionStudioModule extends ReactContextBaseJavaModule {
+
+    private static ReactApplicationContext reactContext;
+
+    private static final String LOG_TAG = "InteractionStudio";
+
+    InteractionStudioModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        //setCampaignHandlers();
+    }
+
+    @Override
+    public String getName() {
+        return "InteractionStudioModule";
+    }
+
+    /**
+     * Adds campaign handlers
+     */
+//    public void setCampaignHandlers() {
+//
+//        Evergage evergage = Evergage.getInstance();
+//        Context context = Evergage.getInstance().getGlobalContext();
+//
+//        /*
+//         * Mobile data campaign handler for Home Banner 1. Make sure the campaign targets pages with the
+//         * View Home action. A JavaScript event is dispatched to the React app when a campaign response is returned.
+//         */
+//        CampaignHandler homeBanner1CampaignHandler = new CampaignHandler() {
+//
+//            @Override
+//            public void handleCampaign(@NonNull Campaign campaign) {
+//
+//                Log.d(LOG_TAG, "Handling campaign: " + campaign.getCampaignName());
+//                context.trackImpression(campaign);
+//                try {
+//                    JSONObject campaignData = campaign.getData();
+//
+//                    WritableMap eventData = new WritableNativeMap();
+//                    // make sure the campaign JSON data contains headerText and imageURL
+//                    eventData.putString("headerText", campaignData.getString("headerText"));
+//                    eventData.putString("imageURL", campaignData.getString("imageURL"));
+//
+//                    // the React app listens for event with name homeBanner1_ready
+//                    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+//                            .emit("homeBanner1_ready", eventData);
+//
+//                } catch (Exception e) {
+//                    Log.d(LOG_TAG, e.getMessage());
+//
+//                }
+//            }
+//        };
+//        // make sure the mobile data campaign has target homeBanner1
+//        context.setCampaignHandler(homeBanner1CampaignHandler, "homeBanner1");
+//    }
+
+    /**
+     * Called when the navigation screen changes. Sends view item, view category and custom actions
+     * to Interaction Studio
+     */
+    @ReactMethod
+    void viewScreen(String screenName, String itemId) {
+
+    try{
+        Evergage evergage = Evergage.getInstance();
+        Context context = Evergage.getInstance().getGlobalContext();
+
+        // Reset any timers
+        context.viewItem(null);
+        context.viewCategory(null);
+
+        // Make sure the process item data from native mobile apps general setting is checked
+        if (screenName.equals("Product")) {
+            context.viewItem(new Product(itemId));
+        } else if (screenName.equals("Category")) {
+            context.viewCategory(new Category(itemId));
+        } else {
+            context.trackAction("View " + screenName);
+        }
+    }
+    catch(Exception e){
+        Log.d("DEBUG", "Context Error:" + e);
+    }
+
+        Log.d(LOG_TAG, "View " + screenName);
+    }
+
+    /**
+     * Called when the add to cart button is pressed. A callback function is invoked once complete.
+     */
+    @ReactMethod
+    void addToCart(String productId, Integer quantity, Callback callback) {
+        Log.d(LOG_TAG, "Product Add to Cart");
+        Evergage evergage = Evergage.getInstance();
+        Context context = Evergage.getInstance().getGlobalContext();
+        Log.d("DEBUG", "Context Error:" + context);
+        // create line item and add to cart
+        context.addToCart(new LineItem(new Product(productId), quantity));
+        callback.invoke();
+    }
+
+}
